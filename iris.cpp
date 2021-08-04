57ebc82ad036d39b66e19aad8393790f7cf5b93f
DanielMsi <88250649+DanielMsi@users.noreply.github.com>
2021-08-04 23:01:33 +0300

    Add files via upload

---
+++ iris.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Daniel on 04/08/2021.
+//
+
+#include "iris.h"
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+using std::cerr;
+using std::cout;
+using std::endl;
+
+#include <fstream>
+
+using std::ifstream;
+using namespace std;
+
+#include <cstdlib>
+#include <sstream>
+#include <string>
+int main(int k, char **path) {
+    // Create a text string, which is used to output the text file
+    string myText;
+    ifstream file(reinterpret_cast<FILE *>(path));
+    // Use a while loop together with the getline() function to read the file line by line
+    while (std::getline(file, myText)) {
+        // Output the text from the file
+        cout << "p";
+        cout << myText;
+        // ליצור מחלקת אירוס
+    }
+    file.close();
     return 0;
-    return y;
 }
